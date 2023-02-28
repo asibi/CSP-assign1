681eb109588b03bef4c50dde890f7a4ca21cade9
Malte Pedersen <maltebp94@gmail.com>
2023-02-28 20:40:47 +0100

    Fixed issues in script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+set -e
+
 # Compile program 
 gcc src/Program.c -o program
+
+mkdir -p results
 
 # Iterate over threads (t) and hash bits (b)
 for t in {1..32}
 do
-    for b in {0..18}    
+    for b in {1..18}    
     do
         # Run program with t and b
         echo "Running: t=$t b=$b"
         algorithm=1
-        ./program algorithm $t $b > results/output-$t-$b.txt # todo specify algorithm 
+        ./program $algorithm $t $b > results/output-$t-$b.txt # todo specify algorithm 
     done
 done
 
