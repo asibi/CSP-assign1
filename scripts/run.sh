1b4db57f162835c6f0ab198dc1f5d74ca05fa4ba
Asibi Ayagiba <asibiayagiba@Asibis-MacBook-Pro.local>
2023-03-01 15:50:20 +0100

    Setup CSV and quick fix buffer size

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,19 +3,30 @@
 set -e
 
 # Compile program 
-gcc src/Program.c -pthread -o program
+gcc -pthread src/Program.c -o program
 
 mkdir -p results
+
+# Setup output file
+echo "algorithm, thread, bits, time, cache-misses, itlb-misses, dtlb-misses" > results/data.txt
 
 # Iterate over threads (t) and hash bits (b)
 for t in {1..32}
 do
     for b in {1..18}    
     do
-        # Run program with t and b
-        echo "Running: t=$t b=$b"
-        algorithm=2
-        ./program $algorithm $t $b > results/output-$t-$b.txt # todo specify algorithm 
+        for runs in {0..8} # Run each configuration 8 times
+        do
+            # 1 = independen out, 2 = count-and-move 
+            algorithm=1
+            echo "Running: a=$algorithm t=$t b=$b"
+            time=$(perf stat -e cache-misses,iTLB-load-misses,dTLB-load-misses ./program $algorithm $t $b 2> perf_tmp.txt) 
+            cache_misses=$(grep cache-misses perf_tmp.txt | sed -E 's/\s+([0-9]+).*$/\1/g')
+            itlb_misses=$(grep iTLB-load-misses perf_tmp.txt | sed -E 's/\s+([0-9]+).*$/\1/g')
+            dtlb_misses=$(grep dTLB-load-misses perf_tmp.txt | sed -E 's/\s+([0-9]+).*$/\1/g')
+            echo "$algorithm, $t, $b, $time, $cache_misses, $itlb_misses, $dtlb_misses" >> results/data.txt
+            rm perf_tmp.txt
+        done
     done
 done
 
--- src/IndependentOut.h
+++ src/IndependentOut.h
@@ -34,7 +34,8 @@ double independent_out(const Tuple* tuples, int num_tuples, int num_threads, int
 
     int partition_count = 1 << num_hash_bits;
     int buffer_count = num_threads * partition_count;
-    int buffer_size = (num_tuples / buffer_count) * 1.5; // 50% extra capacity
+    // todo use this if numbers get uniformly distribued:  1.5; // 50% extra capacity
+    int buffer_size = ((num_tuples + (buffer_count - 1)) / buffer_count) * 10;
     int tuples_per_thread = num_tuples / num_threads; 
     int buffers_per_thread = (buffer_count / num_threads); 
     Buffer **buffers = (Buffer**)malloc(sizeof(Buffer*) * buffer_count);
@@ -70,8 +71,32 @@ double independent_out(const Tuple* tuples, int num_tuples, int num_threads, int
 
     clock_t end = clock();
     double time_spent_ms = (double)(end - begin) / CLOCKS_PER_SEC * 1000.0;
+
+    /* Free memory*/
+    free(buffers);
     
     return time_spent_ms;
+    // /* DEBUG BUFFERS*/
+    // int max = 0;
+    // printf("%d\n", buffer_count);
+    // long histogram[buffer_size+1];
+    // for(int i = 0 ;i < buffer_size; i++) {
+    //     histogram[i] = 0;
+    // }
+    // for ( int i = 0; i < buffer_count; i++){
+    //     histogram[buffers[i]->next]++;
+    //     if(buffers[i]->next > max) {
+    //         // printf("buffer capacity: %d/%d\n", buffers[i]->next, buffers[i]->size);
+    //         max = buffers[i]->next;
+    //     }
+    // }
+    // for(int i = 0 ;i < buffer_size; i++) {
+    //     printf("%d: %ld\n", i, histogram[i]);
+    // }
+    // printf("\n");
+
+
+    
 }
 
 #endif // INDEPENDENT_OUT_H
